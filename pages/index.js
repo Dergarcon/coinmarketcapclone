e017af14876e4d6b9aa9f4cd38ec5e9d073927bd
Nick <nick@om90.de>
2020-09-16 19:21:22 +0200

    fetch and output coin data from coingecko api

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,51 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Coingecko from 'coingecko-api'
 
-export default function Home() {
+const coingeckoClient = new Coingecko()
+
+export default function Home(props) {
+  const {data} = props.res
+  console.log('data: ', data)
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>CMC Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>Coinmarketcap Clone</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <table>
+        <thead>
+          <tr>
+          <th>Symbol</th>
+          <th>Market cap</th>                                  
+          <th>Change (24h)</th>                                  
+          <th>Price</th>            
+          <th>Change (24h)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(coin => 
+            <tr key={coin.id}>
+              <td>{coin.symbol.toUpperCase()}</td>              
+              <td>{coin.market_cap}</td>
+              <td>{coin.market_cap_change_percentage_24h}</td>            
+              <td>{coin.current_price}</td>       
+              <td>{coin.price_change_percentage_24h}</td>       
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   )
+}
+
+// get data from coingecko
+export async function getServerSideProps(){
+  const params = {
+    order: Coingecko.ORDER.MARKET_CAP_DESCENDING
+  }
+  const res = await coingeckoClient.coins.markets({params})
+  return {props: {res}}
 }
